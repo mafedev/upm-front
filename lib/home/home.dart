1b241e75bbb9bf180fdddf77d474d8e42949190e
Mafe <ladrillito966@gmail.com>
2026-02-24 11:32:04 +0100

    feat(serial): implement load sessions flow with password and user input

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -11,6 +11,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final SerialService _serial = SerialService(); // Instancia del servicio
   String _response = "Sin respuesta"; // Es el texto que se muestra al presionar el botón, por defecto es "sin respuesta"
+  final TextEditingController _sessionsController = TextEditingController(); // Es el que permite leer el número de sesiones a cargar
 
   @override
   void initState() {
@@ -35,6 +36,28 @@ class _HomePageState extends State<HomePage> {
   void dispose() {
     _serial.close(); // Cierra el puerto
     super.dispose();
+  }
+
+  // Cargar sesiones
+  void _loadSessions() async {
+    final value = _sessionsController.text.trim(); // obtiene el número de sesiones
+
+    // Si está vacío, muestra el mensaje
+    if (value.isEmpty) {
+      setState(() {
+        _response = "Introduce un número válido";
+      });
+      return;
+    }
+
+    // si no está vacío, envía la contraseña
+    _serial.send("1234");
+
+    // espera un segundo para que entre en el bucle
+    await Future.delayed(const Duration(seconds: 1));
+
+    // y por último envía el número de sesiones a cargar
+    _serial.send(value);
   }
 
   @override
@@ -62,7 +85,32 @@ class _HomePageState extends State<HomePage> {
             // ---------- Ver número de serie ----------
             ElevatedButton(
               onPressed: () =>_serial.send("1"), // Al presionar el botón envia un '1' al Arduino
-            child: const Text("Número de serie"))
+            child: const Text("Número de serie")),
+            
+            const SizedBox(height: 25),
+
+            // ---------- Cargar sesiones ----------
+            const Text("Cargar sesiones", style: TextStyle(fontSize: 16)),
+
+            const SizedBox(height: 10),
+
+            // Campo de texto donde se introducen el número de sesiones a cargar
+            TextField(
+              controller: _sessionsController, // controlador que almacena el número
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Número de sesiones",
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // Cuando se presiona el botón llama a la función para cargar las sesiones
+            ElevatedButton(
+              onPressed: _loadSessions,
+              child: const Text("Enviar sesiones"),
+            ),
           ],
         ),
       ),
